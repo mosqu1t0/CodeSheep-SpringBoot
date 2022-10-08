919efe5ae9fb038e976c2bbc190c89bf57883f89
mosqu1t0 <mosquito@email.cn>
2022-10-08 22:40:16 +0800

    up

--- src/main/java/com/mosquito/codesheep/utils/languageMapUtil.java
+++ src/main/java/com/mosquito/codesheep/utils/languageMapUtil.java
@@ -14,22 +14,26 @@ public class languageMapUtil {
     static {
         suffix.put("c", ".c");
         suffix.put("cpp", ".cpp");
+        suffix.put("rust", ".rs");
         suffix.put("golang", ".go");
         suffix.put("python", ".py");
         suffix.put("javascript", ".js");
 
         command.put("c", "compileC.sh");
         command.put("cpp", "compileCpp.sh");
+        command.put("rust", "compileRust.sh");
         command.put("golang", "compileGo.sh");
         command.put("python", "runPy.sh");
         command.put("javascript", "runJs.sh");
 
         secondCommand.put("c", "runC.sh");
         secondCommand.put("cpp", "runCpp.sh");
+        secondCommand.put("rust", "runRust.sh");
         secondCommand.put("golang", "runGo.sh");
 
         path.put("c", "C/C");
         path.put("cpp", "Cpp/Cpp");
+        path.put("rust", "Rust/Rust");
         path.put("golang", "Go/Go");
         path.put("python", "Py/Py");
         path.put("javascript", "Js/Js");
